d4770ea18e1cc89884abab6a5d7b820b9e2d43cd
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-09-12 10:07:41 -0400

    complete part 1 & 2

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 // Part 2: Deck of Cards
 // 1. Make a request to the Deck of Cards API to request a single card from a newly shuffled deck. Once you have the card, console.log the value and the suit (e.g. “5 of spades”, “queen of diamonds”).
+BASE_API_URL = "http://deckofcardsapi.com/api/deck/";
+newDeck = axios
+  .get(BASE_API_URL + "new/draw/?count=1")
+  .then((response) =>
+    console.log(
+      `${response.data.cards[0].value} of ${response.data.cards[0].suit}`
+    )
+  );
 
 // 2. Make a request to the deck of cards API to request a single card from a newly shuffled deck. Once you have the card, make a request to the same API to get one more card from the same deck.
+let deckID, cardOne, cardTwo;
+
+newDeckTwo = axios
+  .get(BASE_API_URL + "new/draw/?count=1")
+  .then((response) => {
+    cardOne = `${response.data.cards[0].value} of ${response.data.cards[0].suit}`;
+    console.log(
+      `${response.data.cards[0].value} of ${response.data.cards[0].suit}`
+    );
+    deckID = response.data.deck_id;
+    return axios.get(BASE_API_URL + `${deckID}/draw/?count=1`);
+  })
+  .then((response) => {
+    cardTwo = `${response.data.cards[0].value} of ${response.data.cards[0].suit}`;
+    console.log(cardOne, cardTwo);
+  });
 
 // Once you have both cards, console.log the values and suits of both cards.
 
